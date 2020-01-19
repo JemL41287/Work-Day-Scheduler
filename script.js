b33bfca5a03fa9f6af8f2c6a00315ec0edcdf5c5
Jennifer Mullin <jennifer.lindsey.mullin@gmail.com>
2020-01-19 14:40:55 -0700

    created each hour variable and functionality to display current day

--- script.js
+++ script.js
@@ -17,6 +17,41 @@ $(document).ready(function () {
 
     });
 
+    function getData() {
+
+        var hour9 = localStorage.getItem("hour-9");
+        $("#hour-9 .description").text(hour9);
+
+        var hour10 = localStorage.getItem("hour-10");
+        $("#hour-10 .description").text(hour10);
+
+        var hour11 = localStorage.getItem("hour-11");
+        $("#hour-11 .description").text(hour11);
+
+        var hour12 = localStorage.getItem("hour-12");
+        $("#hour-12 .description").text(hour12);
+
+        var hour1 = localStorage.getItem("hour-1");
+        $("#hour-1 .description").text(hour1);
+
+        var hour2 = localStorage.getItem("hour-2");
+        $("#hour-2 .description").text(hour2);
+
+        var hour3 = localStorage.getItem("hour-3");
+        $("#hour-3 .description").text(hour3);
+
+        var hour4 = localStorage.getItem("hour-4");
+        $("#hour-4 .description").text(hour4);
+
+        var hour5 = localStorage.getItem("hour-5");
+        $("#hour-5 .description").text(hour5);
+    }
+
+    var date= newDate();
+    var hours = date.getHours();
+
+
+
 
 
 });
